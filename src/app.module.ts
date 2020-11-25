76a005c94fe2e35ad6f8fe090a179c824d3db8e7
Steve Ghidro <iaghidro@gmail.com>
2020-11-25 18:34:28 -0500

    create lesson module and type

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './lesson/lesson.module';
 @Module({
-  imports: [GraphQLModule.forRoot({ autoSchemaFile: true })],
+  imports: [GraphQLModule.forRoot({ autoSchemaFile: true }), LessonModule],
   controllers: [],
   providers: [],
 })
